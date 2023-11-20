66e095f9b0580276c54455e65eb594f11ed6dd08
Kevin Zhu <kevin-zhu@berkeley.edu>
2023-11-20 12:16:22 +0000

    done with iter1 pt 2

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -4,6 +4,7 @@ class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
   def self.civic_api_to_representative_params(rep_info)
+
     reps = []
 
     rep_info.officials.each_with_index do |official, index|
@@ -19,9 +20,22 @@ class Representative < ApplicationRecord
 
       rep = Representative.find_by(name: official.name, title: title_temp)
       next unless rep.nil?
-
+      address = official.address
+      if address
+        addy = address[0]
+        full_address = [addy&.line1, addy&.line2, addy&.line3, addy&.city, addy&.state, addy&.zip].compact.join(", ")
+      end
+      pt = official.party
+      if pt
+        party = pt
+      end
+      pic = official.photo_url
+      if pic
+        photo_url = pic
+      end
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+          title: title_temp, contact_address: full_address, political_party: party,
+        photo_url: photo_url})
       reps.push(rep)
     end
 
