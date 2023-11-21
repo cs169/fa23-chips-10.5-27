178398f5fa96eef048ea00f6d86e5e3ae5ab1f10
Kevin Zhu <kevin-zhu@berkeley.edu>
2023-11-21 02:40:19 +0000

    fixed lint issues

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -4,9 +4,7 @@ class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
   def self.civic_api_to_representative_params(rep_info)
-
     reps = []
-
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
@@ -20,25 +18,21 @@ class Representative < ApplicationRecord
 
       rep = Representative.find_by(name: official.name, title: title_temp)
       next unless rep.nil?
-      address = official.address
-      if address
-        addy = address[0]
-        full_address = [addy&.line1, addy&.line2, addy&.line3, addy&.city, addy&.state, addy&.zip].compact.join(", ")
-      end
-      pt = official.party
-      if pt
-        party = pt
-      end
-      pic = official.photo_url
-      if pic
-        photo_url = pic
-      end
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp, contact_address: full_address, political_party: party,
-        photo_url: photo_url})
+
+      party = official.party if official.party
+      photo_url = official.photo_url if official.photo_url
+      full_address = extract_full_address(official.address)
+      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp, title: title_temp,
+contact_address: full_address, political_party: party, photo_url: photo_url })
       reps.push(rep)
     end
-
     reps
+  end
+
+  def self.extract_full_address(address)
+    return unless address
+
+    addy = address[0]
+    [addy&.line1, addy&.line2, addy&.line3, addy&.city, addy&.state, addy&.zip].compact.join(', ')
   end
 end
