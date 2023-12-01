51de37dc6bf2268717593e5fc77b6adfdf267fb3
Bill Hu <hubi@berkeley.edu>
2023-12-01 01:28:53 +0000

    fix duplicate code problem

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,17 +3,7 @@
 class CampaignsController < ApplicationController
   def index
     @cycle = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-    @category = {
-      'Candidate Loan'      =>	'candidate-loan',
-      'Contribution Total'  =>	'contribution-total',
-      'Debts Owed'          =>	'debts-owed',
-      'Disbursements Total' =>	'disbursements-total',
-      'End Cash'            =>	'end-cash',
-      'Individual Total'    =>	'individual-total',
-      'PAC Total'           =>	'pac-total',
-      'Receipts Total'      =>	'receipts-total',
-      'Refund Total'        =>	'refund-total'
-    }.keys
+    @category = Campaign::CATEGORIES.keys
   end
 
   def search
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,22 +3,23 @@
 require 'faraday'
 
 class Campaign < ApplicationRecord
+  CATEGORIES = {
+    'Candidate Loan'      =>	'candidate-loan',
+    'Contribution Total'  =>	'contribution-total',
+    'Debts Owed'          =>	'debts-owed',
+    'Disbursements Total' =>	'disbursements-total',
+    'End Cash'            =>	'end-cash',
+    'Individual Total'    =>	'individual-total',
+    'PAC Total'           =>	'pac-total',
+    'Receipts Total'      =>	'receipts-total',
+    'Refund Total'        =>	'refund-total'
+  }.freeze
+
   def self.find_all(prm)
-    categories = {
-      'Candidate Loan'      =>	'candidate-loan',
-      'Contribution Total'  =>	'contribution-total',
-      'Debts Owed'          =>	'debts-owed',
-      'Disbursements Total' =>	'disbursements-total',
-      'End Cash'            =>	'end-cash',
-      'Individual Total'    =>	'individual-total',
-      'PAC Total'           =>	'pac-total',
-      'Receipts Total'      =>	'receipts-total',
-      'Refund Total'        =>	'refund-total'
-    }
     base_url = 'https://api.propublica.org/campaign-finance/v1/'
     api_key = '9lcjslvwVjbqtX0KcQQ3W9rFm316caQQ2T89n4xA'
     request = Faraday.get(
-      "#{base_url}#{prm[:cycle]}/candidates/leaders/#{categories[prm[:category]]}.json", nil, 'X-API-Key' => api_key
+      "#{base_url}#{prm[:cycle]}/candidates/leaders/#{CATEGORIES[prm[:category]]}.json", nil, 'X-API-Key' => api_key
     )
     response = JSON.parse(request.body)
     response['results'].map { |result| result['name'] }
