16ebbc42f52cb257bcd764ce584b99179987a8d5
Kevin Zhu <kevin-zhu@berkeley.edu>
2023-12-01 02:43:57 +0000

    done with tests for rep model and controllers

---
+++ spec/controllers/representatives_controller_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RepresentativesController, type: :controller do
+  describe 'GET #index' do
+    before do
+      get :index
+    end
+
+    it 'responds successfully with 200' do
+      expect(response).to be_sucessful
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index page' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'loads all reps into @representatives' do
+      expect(assigns(:representatives)).to match_array(representatives)
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:representative) do
+      Representative.create!(name: 'John Doe', party: 'Independent', district: '1st', state: 'SomeState')
+    end
+
+    before do
+      get :show, params: { id: representative.id }
+    end
+
+    it 'responds successfully with 200' do
+      expect(response).to be_sucessful
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the show page' do
+      expect(response).to render_template('show')
+    end
+
+    it 'loads reps into @representative' do
+      expect(assigns(:representative)).to eq(representative)
+    end
+
+    it 'raises a RecordNotFound error' do
+      expect do
+        get :show, params: { id: 99 }
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -8,8 +8,21 @@ RSpec.describe Representative, type: :model do
                                    official_indices: [0] }),
                   OpenStruct.new({ name:             'Title2',
                                    official_indices: [1] })],
-      officials: [OpenStruct.new({ name: 'John Doe', title: 'Title1' }),
-                  OpenStruct.new({ name: 'John Doe2', title: 'Title2' })] }
+      officials: [OpenStruct.new({ name:      'John Doe',
+                                   party:     'Republican',
+                                   photo_url: 'http://example.com/john_doe.jpg',
+                                   address:   [OpenStruct.new(line1: '123 Example St',
+                                                              line2: 'Apt 106',
+                                                              city:  'Berkeley',
+                                                              state: 'CA',
+                                                              zip:   '94704')] }),
+                  OpenStruct.new({ name:      'John Doe2',
+                                   party:     'Democrat',
+                                   photo_url: 'http://example.com/john_smith.jpg',
+                                   address:   [OpenStruct.new(line1: '321 Random St',
+                                                              city:  'Los Angeles',
+                                                              state: 'CA',
+                                                              zip:   '91800')] })] }
   )
 
   it "doesn't add duplicates" do
@@ -21,5 +34,25 @@ RSpec.describe Representative, type: :model do
   it 'adds new people' do
     described_class.create(name: 'John Doe3', title: 'Title3')
     expect { described_class.civic_api_to_representative_params(dummy_result) }.to change(described_class, :count).by(2)
+  end
+
+  it 'returns right reps' do
+    expect(described_class.civic_api_to_representative_params(dummy_result).length).to eq(2)
+  end
+
+  describe '.extract_full_address' do
+    context 'when address is present' do
+      let(:address) { dummy_result.officials.first.address }
+
+      it 'returns a formatted address' do
+        expect(described_class.extract_full_address(address)).to eq('123 Example St, Apt 106, Berkeley, CA, 94704')
+      end
+    end
+
+    context 'when address is nil' do
+      it 'returns nil' do
+        expect(described_class.extract_full_address(nil)).to be_nil
+      end
+    end
   end
 end
